dc14ec77d931b321a30046974065c8de68c8dab5
Peter <peterluong1123@gmail.com>
2024-05-26 10:26:23 -0700

    connect form to server

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, send_from_directory, jsonify, request
 from flask_cors import CORS
+import logging
 
-app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s: %(message)s')
+
+# Step 3: Create a logger
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__, static_folder='../react-frontend/build', static_url_path='/')
 CORS(app)
 
 @app.route("/")
-def index():
-    return "<h1>Index Page</h1>"
+def serve_react_app():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    error = None
+    data = request.json
+
+    if data is not None:
+        logger.info(f"Received data: {data}")
+        return jsonify({"message": "Data logged successfully!"}), 200
+    else:
+        logger.error("No data received")
+    # if request.method == 'POST':
+    #     if valid_login(request.form['username'],
+    #                    request.form['password']):
+    #         return log_the_user_in(request.form['username'])
+    #     else:
+    #         error = 'Invalid username/password'
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,28 +1,67 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
 
 function App() {
+  const [formData, setFormData] = useState({
+    title: '',
+    text: '',
+  })
+  // useEffect(() => {
+  //   fetch('http://127.0.0.1:5000/').then(response =>  response.json()).then(data => console.log(data))
+  // })
 
-// useEffect(() => {
-//   fetch('http://127.0.0.1:5000/').then(response =>  response.json()).then(data => console.log(data))
-// })
 
-const handleClick = async () => {
-  try {
-    const response = await fetch('http://127.0.0.1:5000')
-    // const data = await response
-    console.log(response)
-  } catch (error) {
-    console.error(error);
+  const handleClick = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:5000')
+      console.log('Successfully got a response ', response.data)
+    } catch (error) {
+      console.error(error);
+    }
   }
-}
+
+  interface FormData {
+    title: string,
+    text: string,
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // const data = {
+    //   title: e.currentTarget.elements.namedItem('title')?.value || '',
+    //   text: e.currentTarget.elements.namedItem('text')?.value || '',
+    // }
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/login', formData);
+      console.log('Form successfully submitted ', response.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
        <p>My react app with Flask Backend</p>
        <button onClick={handleClick}>Test</button>
+        <form onSubmit={handleSubmit} className='form__container'>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={formData.title}
+            onChange={(e) => { setFormData({...formData, title: e.target.value }) }}
+            />
+          <textarea
+            name="text"
+            placeholder="Text"
+            value={formData.text}
+            onChange={(e) => { setFormData({ ...formData, text: e.target.value })}}
+            />
+        <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
